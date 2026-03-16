656aa3b5e909faf6cee8b0b9745fc2a0a51a917b
James Green <jgreenop22@gmail.com>
2026-03-16 15:36:45 -0700

    Fix bug?

--- src/main/java/shipwrights/genesis_ad_astra/mixin/DimensionSpecialEffectsMixin.java
+++ src/main/java/shipwrights/genesis_ad_astra/mixin/DimensionSpecialEffectsMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = DimensionSpecialEffects.class, remap = false, priority = 100)
+@Mixin(value = DimensionSpecialEffects.class, priority = 100)
 public class DimensionSpecialEffectsMixin {
 
     @Inject(method = "forType", at = @At("HEAD"), cancellable = true)
